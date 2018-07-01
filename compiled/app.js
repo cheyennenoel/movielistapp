ed3ab870c1bade4a0e92b06276b6783c8726b5db
Cheyenne Noel <cheyennenoel@gmail.com>
2018-07-01 16:44:27 -0700

    Implement Webpack

---
+++ compiled/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+require('./main.css');
+
+alert('hello bob');
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL2FwcC5qcyJdLCJuYW1lcyI6WyJhbGVydCJdLCJtYXBwaW5ncyI6Ijs7QUFBQTs7QUFFQUEsTUFBTSxXQUFOIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAnLi9tYWluLmNzcyc7XG5cbmFsZXJ0KCdoZWxsbyBib2InKTsiXX0=
